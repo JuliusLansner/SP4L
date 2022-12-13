5cfb78815672a105469c7379040ffcff2facad45
cph-mh1088 <112551104+cph-mh1088@users.noreply.github.com>
2022-12-13 12:21:48 +0100

    Added obstacles
    Added some test obstacles to use as a reference for collision impact

--- src/MapMaker.java
+++ src/MapMaker.java
@@ -13,6 +13,9 @@ public class MapMaker {
         try {
             tiles[0] = ImageIO.read(getClass().getResourceAsStream("/tiles/HelBane.gif"));
             tiles[1] = ImageIO.read(getClass().getResourceAsStream("/tiles/rødfarve.gif"));
+            tiles[2] = ImageIO.read(getClass().getResourceAsStream("tiles/Wall.gif"));
+            tiles[3] = ImageIO.read(getClass().getResourceAsStream("tiles/testRock.gif"));
+            tiles[4] = ImageIO.read(getClass().getResourceAsStream("tiles/testFlame.gif"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -23,6 +26,11 @@ public class MapMaker {
         loadMapTiles();
 
         g.drawImage(loadMapTiles()[0],0,0,48*20,48*16,null);
+        g.drawImage(loadMapTiles()[2],150,150,6*6,6*6,null);
+        g.drawImage(loadMapTiles()[3],150,650,6*6,6*6,null);
+        g.drawImage(loadMapTiles()[3],800,150,6*6,6*6,null);
+        g.drawImage(loadMapTiles()[3],800,650,6*6,6*6,null);
+        g.drawImage(loadMapTiles()[4],366,75,6*6,6*6,null);
 
         }
 
